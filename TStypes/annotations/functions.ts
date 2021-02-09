5d188d7cf3b967adc4e7d36aef543d6915e441da
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-09 11:52:11 -0500

    Annotation Types around functions

---
+++ TStypes/annotations/functions.ts
@@ -0,0 +1,26 @@
+//define the type of arguments that the function will receive
+// and the type that the function will return
+const add = (a: number, b: number): number => {
+    return a + b
+};
+
+const subtract = (a: number, b: number): number => {
+    return a - b;
+};
+
+function devide(a: number, b: number): number {
+    return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+    return a * b
+}
+
+const logger = (message: string): void => {
+    console.log(message);
+};
+
+//never: means that we never gonna reach the end of the function.
+const throwError = (message: string): never => {
+    throw new Error(message);
+}
