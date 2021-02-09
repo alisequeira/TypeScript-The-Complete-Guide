b7199b75d1d7be7a6793149c84d16fb2df428215
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-09 12:36:51 -0500

    Type arrays: Why do we care?

---
+++ TStypes/arrays.ts
@@ -0,0 +1,23 @@
+//If we do not initialize the array we have to provide a type annotation like this string[]
+//otherwise it's no necessary 
+const carMakers = ['ford', 'toyota', 'chevy'];
+
+const dates = [new Date(), new Date()];
+
+const carsByMake = [
+    ['f150'],
+    ['corolla'],
+    ['camaro']
+];
+
+//Help with inference when extracting values
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+//Prevent incompatible values
+carMakers.push(100);
+
+// Help with map, reduce...etc
+carMakers.map((car: string): string => {
+    return car.toUpperCase();
+});
