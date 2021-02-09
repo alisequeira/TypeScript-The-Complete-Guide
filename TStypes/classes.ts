8f31020df4149ae7a97a7809589a198af3bc8517
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-09 15:30:37 -0500

    Intances method modified

--- TStypes/classes.ts
+++ TStypes/classes.ts
@@ -1,15 +1,25 @@
+/**SUMMARY
+ * Private: can be call it only in the parent class;
+ * protected: can be call it in child class too but not in other places
+ * public: can be call it everywhere
+ */
 class Vehicle {
-    drive(): void {
+    public drive(): void {
         console.log('Chugga chugga');
     };
-    honk(): void {
+    protected honk(): void {
         console.log('beep');
     }
 };
 
+//Here mark an error cuz we cannot overwrite the property public from Vehicle
 class Car extends Vehicle {
-    drive(): void {
+    private drive(): void {
         console.log('vroom');
+    }
+
+    startDriving(): void {
+        this.drive()
     }
 }
 
