937337419c86f0c854a42f21263856c5c7bc8307
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-09 14:47:36 -0500

    Code Reuse with interfaces

--- TStypes/interfaces.ts
+++ TStypes/interfaces.ts
@@ -2,13 +2,30 @@ interface Vehicle {
     name: string;
     year: number;
     broken: boolean;
+    summary(): string;
+}
+
+interface Reported {
+    summary(): string;
 }
 
 const oldCivic = {
     name: 'civic',
     year: 2000,
-    broken: true
+    broken: true,
+    summary(): string {
+        return `Name: ${this.name}`
+    }
 };
+
+const drink = {
+    color: 'brown',
+    carbonated: true,
+    sugar: 40,
+    summary(): string {
+        return `My drink has ${this.sugar} grams of sugar`;
+    }
+}
 
 const printVehicle = (vehicle: Vehicle): void => {
     console.log(`Name: ${vehicle.name}`);
@@ -16,4 +33,10 @@ const printVehicle = (vehicle: Vehicle): void => {
     console.log(`Broken: ${vehicle.broken}`);
 };
 
+const printReport = (item: Reported): void => {
+    console.log(item.summary());
+}
+
 printVehicle(oldCivic);
+printReport(oldCivic);
+printReport(drink);
