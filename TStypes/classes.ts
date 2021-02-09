b55343c13de4e6d73872dafedbb5c199c785980d
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-09 15:09:24 -0500

    Classes

---
+++ TStypes/classes.ts
@@ -0,0 +1,12 @@
+class Vehicle {
+    drive(): void {
+        console.log('Chugga chugga');
+    };
+    honk(): void {
+        console.log('beep');
+    }
+};
+
+const vehicle = new Vehicle();
+vehicle.drive();
+vehicle.honk();
