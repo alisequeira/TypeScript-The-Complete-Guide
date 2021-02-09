a07ad60ba1f162dd24405e6c3cf21000ff786494
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-09 11:58:40 -0500

    Destructuring with Annotations

--- TStypes/annotations/functions.ts
+++ TStypes/annotations/functions.ts
@@ -24,3 +24,16 @@ const logger = (message: string): void => {
 const throwError = (message: string): never => {
     throw new Error(message);
 }
+
+const todayWeather = {
+    date: new Date(),
+    weather: 'sunny'
+};
+
+
+//Destructuring with annotations
+const logWeather = ({ date, weather }: { date: Date, weather: string }): void => {
+    console.log(date);
+    console.log(weather);
+};
+logWeather(todayWeather)
