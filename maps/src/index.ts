81d26a830861982c9cdb05d6d154313a7ac6fcfd
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-10 12:45:44 -0500

    Adding Markers

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,8 @@
 import { User } from './User';
-import { Company } from './Company'
+//import { Company } from './Company'
+import { CustomMap } from './CustomMap';
 
 const user = new User();
-const company = new Company();
+const customMap = new CustomMap('map');
 
-console.log(user);
-console.log(company);
+customMap.addUserMarker(user);
