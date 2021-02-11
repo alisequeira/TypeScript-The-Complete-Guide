cf37451b38b74396c15354e8d8f5493bd7f9e9eb
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-11 12:17:21 -0500

    Sorter Scaffolding

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,11 @@
-console.log('hi there')
+class Sorter {
+    constructor(public collection: number[]) { }
+
+    sort(): void {
+
+    }
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);
+sorter.sort();
+console.log(sorter.collection)
