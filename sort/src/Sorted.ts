c5e59e88d074d6dc55f329cc51eab5dde6b44460
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-15 12:51:32 -0500

    Abstract classes

--- sort/src/Sorted.ts
+++ sort/src/Sorted.ts
@@ -5,16 +5,23 @@ interface Sortable {
     compare(leftIndex: number, rightIdex: number): boolean;
     swap(leftIndex: number, rightIdex: number): void;
 }
-export class Sorter {
-    constructor(public collection: Sortable) { }
+export abstract class Sorter {
+    /**
+     * When we are using abstract class we can provide a set
+     * of method that will be available some time in the future.
+     * todo do that we need to user the key word abstract.
+     */
+    abstract compare(leftIndex: number, rightIdex: number): boolean;
+    abstract swap(leftIndex: number, rightIdex: number): void;
+    abstract length: number;
 
     sort(): void {
-        const { length } = this.collection;
+        const { length } = this;
 
         for (let i = 0; i < length; i++) {
             for (let j = 0; i < length - i - 1; j++) {
-                if (this.collection.compare(j, j + 1)) {
-                    this.collection.swap(j, j + 1);
+                if (this.compare(j, j + 1)) {
+                    this.swap(j, j + 1);
                 }
             }
         }
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,10 @@
 import { Sorter } from './Sorted';
 import { NumberCollection } from './NumberCollection';
 import { CharactersCollection } from './CharacteCollection';
+import { LinkedList } from './LinkedList';
 
 const numberCollection = new NumberCollection([10, 3, -5, 0]);
+/*
 const sorter = new Sorter(numberCollection);
 sorter.sort()
 console.log(numberCollection.data)
@@ -11,3 +13,13 @@ const characterCollection = new CharactersCollection('Xaayb');
 const stringSorter = new Sorter(characterCollection);
 stringSorter.sort();
 console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+const linkedSorter = new Sorter(linkedList);
+linkedSorter.sort();
+linkedList.print();
+*/
