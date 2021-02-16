23142ada63c5fc8484f0fa302c7dd0d4537efcb4
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-16 09:53:12 -0500

    Converting Row to values

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,4 +1,7 @@
 import fs from 'fs';
+import { dateStringToDate } from './utils';
+
+
 export class CsvFileReader {
     data: string[][] = [];
 
@@ -12,6 +15,16 @@ export class CsvFileReader {
             .split('\n')
             .map((row: string): string[] => {
                 return row.split(',');
-            });
+            })
+            .map((row: string[]): any => {
+                return [
+                    dateStringToDate(row[0]),
+                    row[1],
+                    row[2],
+                    parseInt(row[3]),
+                    parseInt(row[4]),
+
+                ]
+            })
     }
 }
