4a8735e901a67090dd9a07b56197530c26a43fa1
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-16 20:40:09 -0500

    Generating html reports

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,5 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var reader = new MatchReader_1.MatchReader('football.csv');
-reader.read();
+var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+var Summary_1 = require("./Summary");
+var HtmlReport_1 = require("./reportTarget/HtmlReport");
+//Create an instance of matchreader  and pass in something 
+//the 'DataReader interface
+var matchReader = new MatchReader_1.MatchReader('football.csv');
+matchReader.read();
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
+summary.buildAndPrintReport(matchReader.data);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,6 +1,18 @@
 import { MatchReader } from './MatchReader';
-import { MatchResult } from './MatchResults';
+import { ConsoleReport } from './reportTarget/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
+import { HtmlReport } from './reportTarget/HtmlReport';
 
 
-const reader = new MatchReader('football.csv');
-reader.read();
+//Create an instance of matchreader  and pass in something 
+//the 'DataReader interface
+const matchReader = new MatchReader('football.csv');
+matchReader.read();
+
+const summary = new Summary(
+    new WinsAnalysis('Man United'),
+    new HtmlReport()
+);
+
+summary.buildAndPrintReport(matchReader.data);
