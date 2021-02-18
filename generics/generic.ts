3c09c93c05342d6f5a89cb55d98649d967b52412
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-18 10:43:34 -0500

    Adnvace Generics

---
+++ generics/generic.ts
@@ -0,0 +1,28 @@
+class ArrayOfNumbers {
+    constructor(public collection: number[]) { }
+
+    get(index: number): number {
+        return this.collection[index];
+    }
+}
+
+class ArrayOfStrings {
+    constructor(public collection: string) { }
+
+    get(index: number): string {
+        return this.collection[index];
+    }
+}
+
+//GENERICS
+
+class ArrayOfAnything<T>{
+    constructor(public collection: T[]) { }
+
+    get(index: number): T {
+        return this.collection[index];
+    }
+}
+
+new ArrayOfAnything<number>([1, 2, 3, 4]);
+new ArrayOfAnything<string>(['a', 'b', 'c']);
