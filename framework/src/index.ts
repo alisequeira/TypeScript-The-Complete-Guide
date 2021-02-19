5402b8169060614df011a774aa581e39b898ceb1
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-19 11:18:36 -0500

    Optional Interface Properties

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -1,5 +1,8 @@
 import { User } from './models/User';
 
 const user = new User({ name: 'Ali', age: 22 });
+
+user.set({ name: 'Jorgell' });
+
 console.log(user.get('name'));
 console.log(user.get('age'));
--- framework/src/models/User.ts
+++ framework/src/models/User.ts
@@ -1,6 +1,9 @@
+//The Question mark means that the property it's optional
+//Now says in order for and object to satisfy this interface it can have a name it can have a name or an age
+//but if it doesn't that's totally okay.
 interface userProps {
-    name: string;
-    age: number
+    name?: string;
+    age?: number
 }
 
 export class User {
@@ -8,5 +11,10 @@ export class User {
 
     get(propName: string): (string | number) {
         return this.data[propName];
+    }
+
+    set(update: userProps): void {
+        //Overwrite all the properties from this.data with the values of update
+        Object.assign(this.data, update);
     }
 }
