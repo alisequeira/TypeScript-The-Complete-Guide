aed7dd990c2ef23848ff44a018db048441c4f6fe
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-22 14:44:53 -0500

    Re-Itegrating Attributes

--- framework/src/models/User.ts
+++ framework/src/models/User.ts
@@ -1,5 +1,8 @@
 import { Eventing } from './Eventing'
 import { Sync } from './Sync';
+import { Attributes } from './Attributes';
+
+
 //The Question mark means that the property it's optional
 //Now says in order for and object to satisfy this interface it can have a name it can have a name or an age
 //but if it doesn't that's totally okay.
@@ -11,11 +14,15 @@ export interface userProps {
 
 const ROOT_URL = 'http://localhost:3000/users'
 export class User {
+    //Two types of initialization 
+
+    //1.When we don't need something coming from the constructor.
     public events: Eventing = new Eventing();
     public sync: Sync<userProps> = new Sync<userProps>(ROOT_URL);
+    public attributes: Attributes<userProps>;
 
-
-
-
-
+    //2. When we got some properties that have to be initialized through the constructor
+    constructor(attrs: userProps) {
+        this.attributes = new Attributes<userProps>(attrs);
+    }
 }
