1a4adcb407db16025c845092191c84fc536b81da
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-22 12:47:51 -0500

    conecting sync back to user

--- framework/src/models/User.ts
+++ framework/src/models/User.ts
@@ -1,5 +1,5 @@
 import { Eventing } from './Eventing'
-
+import { Sync } from './Sync';
 //The Question mark means that the property it's optional
 //Now says in order for and object to satisfy this interface it can have a name it can have a name or an age
 //but if it doesn't that's totally okay.
@@ -8,8 +8,11 @@ export interface userProps {
     name?: string;
     age?: number
 }
+
+const ROOT_URL = 'http://localhost:3000/users'
 export class User {
     public events: Eventing = new Eventing();
+    public sync: Sync<userProps> = new Sync<userProps>(ROOT_URL)
     constructor(private data: userProps) { }
 
     get(propName: string): (string | number) {
