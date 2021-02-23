10225160325ba45cd189680eb7fc8c3156e0d38c
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-23 15:25:17 -0500

    Extracting a View Class

--- framework/src/views/UserForm.ts
+++ framework/src/views/UserForm.ts
@@ -1,14 +1,7 @@
 import { User } from '../models/User';
+import { View } from './View';
+export class UserForm extends View {
 
-export class UserForm {
-
-    constructor(public parent: Element, public model: User) {
-        this.bindModel();
-    }
-
-    bindModel(): void {
-        this.model.on('change', () => this.render());
-    }
     eventsMap(): { [key: string]: () => void } {
         return {
             'click:.set-age': this.onSetAgeClick,
@@ -40,25 +33,5 @@ export class UserForm {
         `;
     }
 
-    bindEvents(fragment: DocumentFragment): void {
-        const eventsMap = this.eventsMap();
 
-        for (let eventsKey in eventsMap) {
-            const [eventName, selector] = eventsKey.split(':');
-
-            fragment.querySelectorAll(selector).forEach(element => {
-                element.addEventListener(eventName, eventsMap[eventsKey]);
-            })
-        }
-    }
-
-    render(): void {
-        this.parent.innerHTML = '';
-
-        const templateElement = document.createElement('template');
-        templateElement.innerHTML = this.template();
-
-        this.bindEvents(templateElement.content);
-        this.parent.append(templateElement.content)
-    }
 }
