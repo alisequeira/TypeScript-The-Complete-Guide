86f86376204126e3ca1083d7965f81dc51c71728
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-23 11:35:03 -0500

    Parsing User Json

--- framework/src/models/Collection.ts
+++ framework/src/models/Collection.ts
@@ -1,8 +1,12 @@
-import { User } from './User';
-import { Eventing } from './Eventing'
+import axios, { AxiosResponse } from 'axios'
+import { User, userProps } from './User';
+import { Eventing } from './Eventing';
+
 export class Collection {
     models: User[] = [];
     events: Eventing = new Eventing();
+
+    constructor(public root_URL: string) { }
 
     get on() {
         return this.events.on;
@@ -10,5 +14,16 @@ export class Collection {
 
     get trigger() {
         return this.events.trigger;
+    }
+
+    fetch(): void {
+        axios.get(this.root_URL)
+            .then((response: AxiosResponse) => {
+                response.data.forEach((value: userProps) => {
+                    const user = User.buildUser(value);
+                    this.models.push(user);
+                });
+                this.trigger('change')
+            });
     }
 }
