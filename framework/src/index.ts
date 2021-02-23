f1a14361322a96b7f7ea84f0908807cc1a6c39e5
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-23 10:42:31 -0500

    Extending The User

--- framework/src/index.ts
+++ framework/src/index.ts
@@ -2,8 +2,8 @@ import { User } from './models/User';
 
 const user = new User({ id: 1, name: 'Ali Obregon', age: 20 });
 
-user.on('save', () => {
-    console.log(user);
-});
+//user.on('save', () => {
+    //console.log(user);
+//});
 
-user.save();
+//user.save();
--- framework/src/models/User.ts
+++ framework/src/models/User.ts
@@ -1,7 +1,4 @@
-import { Eventing } from './Eventing'
-import { Sync } from './Sync';
-import { Attributes } from './Attributes';
-import { AxiosResponse } from 'axios';
+import { Model } from './Model';
 
 
 //The Question mark means that the property it's optional
@@ -14,56 +11,6 @@ export interface userProps {
 }
 
 const ROOT_URL = 'http://localhost:3000/users'
-export class User {
-    //Two types of initialization 
+export class User extends Model<userProps> {
 
-    //1.When we don't need something coming from the constructor.
-    public events: Eventing = new Eventing();
-    public sync: Sync<userProps> = new Sync<userProps>(ROOT_URL);
-    public attributes: Attributes<userProps>;
-
-    //2. When we got some properties that have to be initialized through the constructor
-    constructor(attrs: userProps) {
-        this.attributes = new Attributes<userProps>(attrs);
-    }
-
-    get on() {
-        //Return a reference to the on event
-        return this.events.on;
-    }
-    get trigger() {
-        return this.events.trigger;
-    }
-
-    get get() {
-        return this.attributes.get;
-    }
-
-    set(update: userProps): void {
-        this.attributes.set(update);
-        this.events.trigger('change');
-    }
-
-    fetch(): void {
-        const id = this.attributes.get('id');
-
-        if (typeof id !== 'number') {
-            throw new Error('Cannot fetch without an id');
-        }
-
-        this.sync.fetch(id)
-            .then((response: AxiosResponse): void => {
-                this.set(response.data);
-            });
-    }
-
-    save(): void {
-        this.sync.save(this.attributes.getAll())
-            .then((response: AxiosResponse): void => {
-                this.trigger('save');
-            })
-            .catch(() => {
-                this.trigger('error');
-            })
-    }
 }
