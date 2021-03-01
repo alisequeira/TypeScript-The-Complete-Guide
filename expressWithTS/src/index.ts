c0d67c1c806ce284db98ea62c6c518b489332183
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-01 11:21:01 -0500

    Express Basic Routing

--- expressWithTS/src/index.ts
+++ expressWithTS/src/index.ts
@@ -1,1 +1,12 @@
-console.log('hi there')
+import express, { Request, Response } from 'express';
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send(`
+        <div>
+            <h1>Hi There!</h1>
+        </div>    
+    `)
+});
+
+app.listen(3000, () => console.log('server listening at port 3000'))
