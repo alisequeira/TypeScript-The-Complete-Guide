50f9205c8c6bf82379b8c6f52233113412b52b9f
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-03-03 13:16:29 -0500

    Actions types enum

--- ts-react/src/actions/index.ts
+++ ts-react/src/actions/index.ts
@@ -1,14 +1,23 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
+import { ActionsTypes } from './types';
 
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
+interface Todo {
+    id: number,
+    title: string,
+    complete: boolean
+}
+
+
+
 export const fetchTodos = () => {
     return async (dispatch: Dispatch) => {
-        const response = await axios.get(url);
+        const response = await axios.get<Todo[]>(url);
 
         dispatch({
-            type: 'FETCH_TODOS',
+            type: ActionsTypes.fetchTodos,
             payload: response.data
         });
     };
